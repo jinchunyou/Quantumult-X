7b332d02789f783370b5fdb68970997759a1dc10
jinchunyou <31650821+jinchunyou@users.noreply.github.com>
2024-07-16 22:37:51 +0800

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+// QuantumultX Script to check SSID and redirect
+const ssid = $network.wifi.ssid;
+const targetSSID = "Apple"; // Replace with your SSID
+
+if (ssid === targetSSID) {
+    const newUrl = "http://192.168.1.215" + $request.path;
+    $done({ response: { status: 302, headers: { Location: newUrl } } });
+} else {
+    $done({});
+}
